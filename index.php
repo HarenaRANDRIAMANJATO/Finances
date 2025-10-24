2c3dc9d9c3dfe537d97fe72e7700751868db6a97
Harena RANDRIAMANJATO <r.harena26@gmail.com>
2025-10-24 05:33:10 +0300

    accueil

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+<meta charset="UTF-8">
+<title>Accueil LF 2025</title>
+<link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css" />
+<link rel="stylesheet" href="../assets/bootstrap/css/bootstrap-icons.css" />
+<link rel="stylesheet" href="../assets/bootstrap/styles/style.css" />
+<script src="../assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+<div class="container mt-5">
+    <h1 class="mb-4 text-center">LF 2025 - Finance</h1>
+
+    <div class="row justify-content-center">
+        <div class="col-md-3 mb-3">
+            <a href="recette.php" class="btn btn-primary btn-lg w-100">
+                <i class="bi bi-cash-stack"></i> Recettes
+            </a>
+        </div>
+        <div class="col-md-3 mb-3">
+            <a href="depense.php" class="btn btn-success btn-lg w-100">
+                <i class="bi bi-wallet2"></i> Dépenses
+            </a>
+        </div>
+        <div class="col-md-3 mb-3">
+            <a href="don.php" class="btn btn-warning btn-lg w-100">
+                <i class="bi bi-gift"></i> Dons
+            </a>
+        </div>
+        <div class="col-md-3 mb-3">
+            <a href="deficit.php" class="btn btn-danger btn-lg w-100">
+                <i class="bi bi-file-earmark-bar-graph"></i> Déficits
+            </a>
+        </div>
+    </div>
+</div>
+</body>
+</html>
